7144a25fe0262d2e30331491913e9c94b4ad4060
iyagikkun <mailforacccreation@gmail.com>
2023-12-31 14:16:30 +0900

    check circular dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const CircularDependencyPlugin = require('circular-dependency-plugin')
 
 module.exports = {
   mode: 'development',
@@ -10,30 +13,35 @@ module.exports = {
       {
         test: /\.ts$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
+        exclude: /node_modules/
+      }
+    ]
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: ['.ts', '.js']
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-    new HtmlWebpackPlugin({ 
-      template: "src/index.html",
-      inject: "body"
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: 'body'
     }),
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src/darkseoul/object'),
-          to: path.resolve(__dirname, 'dist/assets/object'),
-          filter: (resourcePath) => /.*\.png$/.test(resourcePath),
-        },
-      ],
+          from: path.resolve(__dirname, 'src/darkseoul'),
+          to: path.resolve(__dirname, 'dist/assets'),
+          filter: (resourcePath) => /.*\.png$/.test(resourcePath)
+        }
+      ]
     }),
+    new CircularDependencyPlugin({
+      // options
+      exclude: /a\.js|node_modules/, // exclude node_modules
+      failOnError: true // fail the build on circular dependency
+    })
   ],
   devServer: {
     compress: true,
