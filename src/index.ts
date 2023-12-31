3585be39016f7c5dabf4ea1d06e07a036b3503b7
iyagikkun <mailforacccreation@gmail.com>
2023-12-31 16:30:45 +0900

    u

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import darkseoul from './darkseoul'
+import Iyagi from 'iyagi'
+import title from './darkseoul/scene/title'
 
 function init () {
-  darkseoul.play()
+  const canvas = document.querySelector<HTMLCanvasElement>('#display')
+  if (canvas == null) {
+    throw new Error('Fail to init. no the canvas.')
+  }
+
+  Iyagi.set(canvas, title, { debug: true })
+  Iyagi.play()
 }
 
 init()
